95738086a05c1b2b90a7bb8ee426b95223123487
jerome <zherong.teo@gmail.com>
2025-01-09 01:39:17 -0800

    transition day/night implemented

--- src/lib/components/hero/Background.svelte
+++ src/lib/components/hero/Background.svelte
@@ -3,6 +3,74 @@
 	import Logo from '$lib/media/QWERHACKS_Logo.svg';
 	import Shrubs from '$lib/media/Shrubs.svg';
 	import Hill from '$lib/media/Hill.svg';
+	import Hill_Day from '$lib/media/hill_day.svg';
+	import Hill_Night from '$lib/media/hill_night.svg';
+
+	const getPSTTime = () => {
+		const now = new Date();
+		const utcTime = now.getTime() + now.getTimezoneOffset() * 60000;
+		const pstOffset = -8 * 60 * 60000; // PST offset in milliseconds
+		return new Date(utcTime + pstOffset);
+	};
+
+	// Reactive variables
+	let currentTime = getPSTTime();
+	let transitionProgress = 0; // 0 = fully day, 1 = fully night
+
+	const updateTransition = () => {
+		const hours = currentTime.getHours(); // PST hours
+		const minutes = currentTime.getMinutes(); // PST minutes
+
+		if (hours === 17) {
+			// Transition from light to dark (5 PM to 6 PM)
+			transitionProgress = minutes / 60; // Progress from 0 to 1
+		} else if (hours === 8) {
+			// Transition from dark to light (8 AM to 9 AM)
+			transitionProgress = 1 - minutes / 60; // Progress from 1 to 0
+		} else if (hours > 17 || hours < 8) {
+			// Fully night (after 6 PM until 8 AM)
+			transitionProgress = 1;
+		} else {
+			// Fully day (after 9 AM until 5 PM)
+			transitionProgress = 0;
+		}
+
+		console.log(`Transition Progress: ${transitionProgress}`);
+	};
+
+	// Update every minute
+	setInterval(() => {
+		currentTime = getPSTTime(); // Recalculate PST time
+		updateTransition(); // Update transition progress
+	}, 60000);
+
+	// Initial calculation
+	updateTransition();
+
+	const interpolateColor = (startColor, endColor, progress) => {
+		// Parse HEX colors to RGB
+		const start = {
+			r: parseInt(startColor.slice(1, 3), 16),
+			g: parseInt(startColor.slice(3, 5), 16),
+			b: parseInt(startColor.slice(5, 7), 16)
+		};
+		const end = {
+			r: parseInt(endColor.slice(1, 3), 16),
+			g: parseInt(endColor.slice(3, 5), 16),
+			b: parseInt(endColor.slice(5, 7), 16)
+		};
+
+		// Interpolate each channel
+		const r = Math.round(start.r + (end.r - start.r) * progress);
+		const g = Math.round(start.g + (end.g - start.g) * progress);
+		const b = Math.round(start.b + (end.b - start.b) * progress);
+
+		// Return the interpolated color in HEX format
+		return `rgb(${r}, ${g}, ${b})`;
+	};
+
+	// Reactive background color
+	$: backgroundColor = interpolateColor('#92ceff', '#4162BF', transitionProgress);
 </script>
 
 <!-- <div class="full-background">
@@ -15,15 +83,28 @@
 		<h1 class="june text-white">Feb 2-4, 2025</h1>
 	</div>
 </div> -->
-<div class="full-background">
-	<img src={Hill} alt="hill" class="w-full top-hill" />
+<div class="full-background" style="background-color: {backgroundColor}">
+	<!-- <img src={Hill} alt="hill" class="w-full top-hill" /> -->
+	<img
+		src={Hill_Day}
+		alt="hill"
+		class="w-full top-hill day"
+		style="opacity: {1 - transitionProgress}"
+	/>
+	<img
+		src={Hill_Night}
+		alt="hill"
+		class="w-full top-hill night"
+		style="opacity: {transitionProgress}"
+	/>
+
 	<div class="logo-container">
 		<div class="greeting-text">
 			<h1 class="june text-white">Greetings From</h1>
 		</div>
 		<img src={Logo} alt="logo" class="qwerhacks-logo" />
 		<div class="date-text">
-			<h1 class="june text-white">Feb 2-4, 2025</h1>
+			<h1 class="june text-white">Feb 2-4, 2025 {transitionProgress}</h1>
 		</div>
 	</div>
 	<img src={Shrubs} alt="shrubs" class="shrubs" />
@@ -35,6 +116,11 @@
 		top: 10%;
 		bottom: 0px; /* Adjust placement */
 		right: 0px; /* Adjust placement */
+	}
+	.day {
+		z-index: 2;
+	}
+	.night {
 		z-index: 1;
 	}
 	@import url(https://fonts.adobe.com/fonts/june-expt-variable?vf-axes=STYL%2C63&vf-font-size=100&vf-font=JuneExptVariable-Active);
@@ -43,15 +129,15 @@
 		font-variation-settings: 'STYL' 20;
 		font-size: 4vw;
 	}
-
 	.full-background {
-		background-color: #92ceff; /* Set background to #92ceff */
+		// background-color: #92ceff; /* Set background to #92ceff */
 		width: 100%;
 		height: 100%;
 		position: absolute;
 		top: 0;
 		left: 0;
 		z-index: 0;
+		transition: background-color 1s linear; /* Smooth color transition */
 	}
 
 	.date-text,
