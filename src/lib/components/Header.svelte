fae26cc6c693cb9eaab47bb82090dbab8ad86462
Alex Su <suziyue@gmail.com>
2025-01-21 01:07:57 -0800

    Adjusted navbar formatting

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
 	import { onMount, tick } from 'svelte';
 	import { scrollTo, scrollTop } from 'svelte-scrolling';
-	import RetroButton from './utils/RetroButton.svelte';
-	import CleanButton from './utils/CleanButton.svelte';
 	import SleekButton from './utils/SleekButton.svelte';
+	import OvalButton from './utils/OvalButton.svelte';
 
 	const formatTwoDigit = (n: number) => {
 		return n < 10 ? `0${n}` : `${n}`;
@@ -31,29 +30,29 @@
 </script>
 
 <div
-	class="hidden sm:flex bg-lavender fixed top-0 left-0 z-[200] w-full h-[80px] px-4 py-10 align-center whiteborder"
+	class="hidden sm:flex fixed top-0 left-0 z-[200] w-full h-[80px] px-4 py-10 align-center whiteborder"
 	bind:this={headerElement}
 >
 	<div class="flex flex-row line w-[100%]" role="navigation">
 		<div class="flex flex-row items-center gap-1 sm:gap-2 md:gap-2">
 			<SleekButton>
-				<button on:click={() => scrollTop()}>home</button>
+				<button on:click={() => scrollTop()}>Home</button>
 			</SleekButton>
 			<SleekButton>
-				<button on:click={() => scrollTop()}>about</button>
+				<button on:click={() => scrollTop()}>About</button>
 			</SleekButton>
 			<SleekButton>
-				<button on:click={() => scrollTop()}>tracks</button>
+				<button on:click={() => scrollTop()}>Tracks</button>
 			</SleekButton>
 			<SleekButton>
 				<button on:click={() => scrollTop()}>FAQ</button>
 			</SleekButton>
 			<SleekButton>
-				<button on:click={() => scrollTop()}>sponsors</button>
+				<button on:click={() => scrollTop()}>Sponsors</button>
 			</SleekButton>
-			<RetroButton classList="active">
+			<OvalButton>
 				<a class="apply" href="/apply">Apply Now!</a>
-			</RetroButton>
+			</OvalButton>
 		</div>
 	</div>
 </div>
---
+++ src/lib/components/utils/OvalButton.svelte
@@ -0,0 +1,15 @@
+<div class="font-etna">
+    <slot/>
+</div>
+
+<style lang="scss">
+    :global(.apply) {
+        background-color: #f94a9c;
+        padding-top: 0.5rem;
+        padding-bottom: 0.5rem;
+        padding-left: 1.5rem;
+        padding-right: 1.5rem;
+        border-radius: 1.5rem;
+        margin: 0.5rem;
+    }
+</style>
