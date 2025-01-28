a03c8be0879229184ac48635d90cc0ceb4b8e889
Alex Su <suziyue@gmail.com>
2025-01-28 15:22:52 -0800

    Changed background opacity

--- src/lib/components/utils/Carousel.svelte
+++ src/lib/components/utils/Carousel.svelte
@@ -175,6 +175,7 @@
 		box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
 		user-select: none; /* Prevent image selection during drag */
 		-webkit-user-drag: none; /* Prevent image dragging */
+		background-color: rgba(255, 255, 255, 0.8);
 	}
 
 	/* Center image */
